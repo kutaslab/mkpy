57f4e606cd87c53f5ea8ed2f075936c0d269c10a
Tom Urbach <turbach@ucsd.edu>
2019-10-27 09:36:09 -0700

    added pytest to screen version string

--- tests/test_mkpy.py
+++ tests/test_mkpy.py
@@ -1,7 +1,11 @@
-import re
 from pathlib import Path
 import mkpy
 from mkpy import get_ver
+
+
+def test_mkpy__init__version():
+    # screen version for updates
+    assert mkpy.__version__ == "0.1.6"
 
 
 def test_mkpy__init__getver():
